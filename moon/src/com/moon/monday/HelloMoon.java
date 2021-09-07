39c2fdda8c950d21458c8cdd8837cf2d8de06bae
RAM <ramesh9841387544@gmail.com>
2021-09-06 20:51:05 -0400

    a project.

---
+++ moon/src/com/moon/monday/HelloMoon.java
@@ -0,0 +1,14 @@
+package com.moon.monday;
+
+public class HelloMoon {
+
+	public static void main(String[] args) {
+		
+		int num=4;
+		for(int i=1; i<=num; i++) {
+			System.out.println("Moon has no living beings.......");
+		}
+
+	}
+
+}
